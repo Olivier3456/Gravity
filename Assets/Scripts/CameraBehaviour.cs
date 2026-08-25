b41325ae58060d6b9e724eadf75bed2d85712609
Olivier3456 <olivier.larrey@gmail.com>
2026-08-25 09:03:42 +0200

    WIP camera behaviour.

---
+++ Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraBehaviour : MonoBehaviour
+{
+
+    [SerializeField] private float cameraPositionSpeed = 1f;
+    [SerializeField] private float cameraScrollSpeed = 1f;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        // var delta = Mouse.current.delta;
+        float mouseDeltaX = Mouse.current.delta.x.value;
+        float mouseDeltaY = Mouse.current.delta.y.value;
+
+        Camera.main.transform.position += cameraPositionSpeed * new Vector3(mouseDeltaX, mouseDeltaY, 0f);
+
+        Camera.main.orthographicSize += cameraScrollSpeed * Mouse.current.scroll.value.y;
+        // Debug.Log(Mouse.current.scroll.value);
+    }
+}
